abdc6140856b2026323eba228e2e29405b739898
Camiloj896 <Camiloj896@gmail.com>
2019-04-09 18:09:51 -0500

    se sube el mvc

---
+++ 03MVC/Views/template.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>template</title>
+</head>
+<body>
+
+<header>
+    <h1> LOGO TIPO</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Nosotros</a></li>
+        <li><a href="">Servivios</a></li>
+        <li><a href="">Contactenos</a></li>
+    </ul>
+
+</nav>
+<section>
+
+    <h1>Pagina de inicio </h1>
+
+</section>
+
+</body>
+
+</html>
---
+++ 03MVC/index.php
@@ -0,0 +1,11 @@
+<?php
+/*  El Index: en el mostraremos la salida de las  vistas al usuario  y tambien atraves
+
+de el enviaremos las distintas acciones que el usuario enie al controlador 
+*/
+
+
+
+
+
+?>
